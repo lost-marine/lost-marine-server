57862692adff44b9f105cf40e1c8782192c34d74
양소영 <48993188+YangSSo51@users.noreply.github.com>
2024-03-06 10:57:30 +0900

    Feat: 생명체, 플레이어 클래스 생성

---
+++ server/src/classes/player.ts
@@ -0,0 +1,29 @@
+import { Creature } from "./Creature";
+
+export class Player extends Creature {
+  startX: number = 0;
+  startY: number = 0;
+  direction: number = 2;
+  health: number = 100;
+  type: number = 1;
+  power: number = 1;
+  status: number = 1;
+  playerId: number = 0;
+  nickname: string = "";
+  planktonCount: number = 0;
+  microplasticCount: number = 0;
+  playerCount: number = 0;
+  stopTime: number = 0;
+  level: number = 0;
+  experience: number = 0;
+  socketId: string = "";
+
+  constructor(playerId: number, nickname: string, startX: number, startY: number, socketId: string) {
+    super();
+    this.playerId = playerId;
+    this.nickname = nickname;
+    this.startX = startX;
+    this.startY = startY;
+    this.socketId = socketId;
+  }
+}
